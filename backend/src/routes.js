749ef0ad9378dc08ad6e9edb9fbfa422db7606d4
André Tavares <andretavares3@gmail.com>
2019-10-23 15:23:25 -0300

    Criando Dashboard Controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
+
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -10,7 +12,10 @@ const upload = multer(uploadConfig);
 routes.get('/sessions', SessionController.index);
 routes.post('/sessions', SessionController.store);
 
+routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail') , SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
 
 
 module.exports = routes;
