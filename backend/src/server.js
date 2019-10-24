2b2bc7b30ddf34dcf8a920e15696c21cccedf81d
André Tavares <andretavares3@gmail.com>
2019-10-24 10:35:57 -0300

    FIX: added new folders

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -12,6 +14,7 @@ mongoose.connect('mongodb+srv://andre:andre@restfulapi-k8xmw.mongodb.net/apiAirC
 // req.params = Acessar route params (para edição, delete)
 // req.body =  Acessar corpo da requisição (para criação e edição)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
