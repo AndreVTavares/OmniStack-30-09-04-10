0dff1247c2fe59358b2d939d67b8d7e973cf17b6
André Tavares <andretavares3@gmail.com>
2019-10-24 14:55:13 -0300

    Fix: thumbnail

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://andre:andre@restfulapi-k8xmw.mongodb.net/apiAirC
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')));
 app.use(routes);
 
 
