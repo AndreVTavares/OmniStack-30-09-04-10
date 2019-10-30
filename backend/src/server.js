936fc88e26de1a6ebcec309ac586004b3fe74339
André Tavares <andretavares3@gmail.com>
2019-10-30 13:48:53 -0300

    socket IO implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,37 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+
 
 mongoose.connect('mongodb+srv://andre:andre@restfulapi-k8xmw.mongodb.net/apiAirCnC?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true })
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    console.log(socket.handshake.query);
+    console.log('Usuário conectado', socket.id);
+
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+    console.log(connectedUsers);
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
 
 // GET, POST, PUT, DELETE
 
@@ -21,4 +47,4 @@ app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')));
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
